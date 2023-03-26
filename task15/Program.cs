150da1d7b43d59725a6deb4d35c4238c6fa8bc62
Gennady <genchik111@yandex.ru>
2023-03-26 13:42:13 +0300

    solution task15

---
+++ task15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number == 6 || number == 7)
+{
+   Console.Write($"{number} -> выходной, бегом за пивом");
+}
+else if (number >= 1 && number <= 5)
+{
+   Console.Write($"{number} -> рабочий день, соберись, тряпка");
+}
+else
+{
+   Console.Write($"{number} -> такого дня недели не существует, видимо, ты в Нарнии");
+}
+
