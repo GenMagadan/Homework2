15e5ea1dc38aafe7e5d7d12ed6acf0916cc7ba74
Gennady <genchik111@yandex.ru>
2023-03-26 13:32:42 +0300

    solution task13

---
+++ task13/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите любое число: ");
+int number = int.Parse(Console.ReadLine());
+int tempNumb = number;
+if (number > 99)
+{
+   while (number > 999)
+   {
+      number = number / 10;
+   }
+   Console.Write($"{tempNumb} -> третья цифра {number % 10}");
+}
+else
+{
+   Console.Write($"{number} -> третьей цифры нет");
+}
+
+
+
+
+
+
+
+
